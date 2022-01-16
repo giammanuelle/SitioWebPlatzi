eea50b4d9962d09d90e8d1b0e9286fb09a7ab681
giammanuelle <giamgordon@gmail.com>
2022-01-16 16:43:59 -0500

    poryecto integrado

---
+++ PlatziWEBsite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatziWEBsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
